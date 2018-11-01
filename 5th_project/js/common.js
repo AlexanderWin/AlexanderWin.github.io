e5febfdcefafb6f62844731441601eb66a6c768d
AlexanderWin <alex.win.portfolio@gmail.com>
2018-11-02 00:27:22 +0300

    e

--- 5th_project/js/common.js
+++ 5th_project/js/common.js
@@ -1,95 +1,257 @@
-//DRAW LINES
+//DRAW LINES RIGHT UPPER
 
 var canvas = document.getElementById('canvas').getContext('2d'),
     drawMap = [
         {
-            moveTo: [20,20],
-            lineTo: [30,30],
+            moveTo: [1200,380],
+            lineTo: [1220,380],
             t: [2000]
         },
         {
-            moveTo: [40,40],
-            lineTo: [50,50],
+            moveTo: [1230,380],
+            lineTo: [1250,380],
             t: [2000]
         },
         {
-            moveTo: [60,60],
-            lineTo: [70,70],
+            moveTo: [1260,380],
+            lineTo: [1280,380],
             t: [2000]
         },
         {
-            moveTo: [80,80],
-            lineTo: [90,90],
+            moveTo: [1290,380],
+            lineTo: [1310,380],
             t: [2000]
         }, 
         {
-            moveTo: [100,100],
-            lineTo: [110,110],
+            moveTo: [1320,370],
+            lineTo: [1330,360],
             t: [2000]
-        }
-
-    
+        },    
+        {
+            moveTo: [1340,350],
+            lineTo: [1350,340],
+            t: [2000]
+        },
+        {
+            moveTo: [1360,330],
+            lineTo: [1370,320],
+            t: [2000]
+        }, 
+        {
+            moveTo: [1380,310],
+            lineTo: [1390,300],
+            t: [2000]
+        }    
     ],
+
   tStart = 500;
     
 drawMap.forEach(function(el, i) {
-    setTimeout(function() {
+    setTimeout(function() {                     
         canvas.moveTo(el.moveTo[0], el.moveTo[1]);
-        canvas.lineTo(el.lineTo[0], el.lineTo[1]);
+        canvas.lineTo(el.lineTo[0], el.lineTo[1]);        
         canvas.stroke();
         console.log(i);
     }, 
   //el.t
     tStart
   );
-  tStart += 1000;
+  tStart += 120;
 });
+
+//DRAW LINES RIGHT LOWER
 
 var canvas = document.getElementById('canvas').getContext('2d'),
     drawMap = [
         {
-            moveTo: [1200, 500],
-            lineTo: [1100, 500],
+            moveTo: [1200,500],
+            lineTo: [1220,500],
             t: [2000]
         },
         {
-            moveTo: [240,240],
-            lineTo: [250,250],
+            moveTo: [1230,500],
+            lineTo: [1250,500],
             t: [2000]
         },
         {
-            moveTo: [260,260],
-            lineTo: [270,270],
+            moveTo: [1260,500],
+            lineTo: [1280,500],
             t: [2000]
         },
         {
-            moveTo: [280,280],
-            lineTo: [290,290],
+            moveTo: [1290,500],
+            lineTo: [1310,500],
             t: [2000]
         }, 
         {
-            moveTo: [200,200],
-            lineTo: [210,210],
+            moveTo: [1320,510],
+            lineTo: [1330,520],
             t: [2000]
-        }
-
-    
+        },    
+        {
+            moveTo: [1340,530],
+            lineTo: [1350,540],
+            t: [2000]
+        },
+        {
+            moveTo: [1360,550],
+            lineTo: [1370,560],
+            t: [2000]
+        }, 
+        {
+            moveTo: [1380,570],
+            lineTo: [1390,580],
+            t: [2000]
+        }    
     ],
+
   tStart = 500;
     
 drawMap.forEach(function(el, i) {
-    setTimeout(function() {
+    setTimeout(function() {                
         canvas.moveTo(el.moveTo[0], el.moveTo[1]);
-        canvas.lineTo(el.lineTo[0], el.lineTo[1]);
+        canvas.lineTo(el.lineTo[0], el.lineTo[1]);        
         canvas.stroke();
         console.log(i);
     }, 
   //el.t
     tStart
   );
-  tStart += 1000;
+  tStart += 120;
 });
 
+//DRAWING LINES LEFT UPPER
+
+var canvas = document.getElementById('canvas').getContext('2d'),
+    drawMap = [
+        {
+            moveTo: [680,380],
+            lineTo: [700,380],
+            t: [2000]
+        },
+        {
+            moveTo: [670,380],
+            lineTo: [650,380],
+            t: [2000]
+        },
+        {
+            moveTo: [640,380],
+            lineTo: [620,380],
+            t: [2000]
+        },
+        {
+            moveTo: [610,380],
+            lineTo: [590,380],
+            t: [2000]
+        }, 
+        {
+            moveTo: [580,370],
+            lineTo: [570,360],
+            t: [2000]
+        },    
+        {
+            moveTo: [560,350],
+            lineTo: [550,340],
+            t: [2000]
+        },
+        {
+            moveTo: [540,330],
+            lineTo: [530,320],
+            t: [2000]
+        }, 
+        {
+            moveTo: [520,310],
+            lineTo: [510,300],
+            t: [2000]
+        }    
+    ],
+    
+  tStart = 500;
+    
+drawMap.forEach(function(el, i) {
+    setTimeout(function() {   
+
+        canvas.moveTo(el.moveTo[0], el.moveTo[1]);
+        canvas.lineTo(el.lineTo[0], el.lineTo[1]);        
+        canvas.stroke();
+        console.log(i);
+    }, 
+  //el.t
+    tStart
+  );
+  tStart += 120;
+});
+
+//DRAWING LINES LEFT LOWER
+
+var canvas = document.getElementById('canvas').getContext('2d'),
+    drawMap = [
+        {
+            moveTo: [680,500],
+            lineTo: [700,500],
+            t: [2000]
+        },
+        {
+            moveTo: [670,500],
+            lineTo: [650,500],
+            t: [2000]
+        },
+        {
+            moveTo: [640,500],
+            lineTo: [620,500],
+            t: [2000]
+        },
+        {
+            moveTo: [610,500],
+            lineTo: [590,500],
+            t: [2000]
+        }, 
+        {
+            moveTo: [580,510],
+            lineTo: [570,520],
+            t: [2000]
+        },    
+        {
+            moveTo: [560,530],
+            lineTo: [550,540],
+            t: [2000]
+        },
+        {
+            moveTo: [540,550],
+            lineTo: [530,560],
+            t: [2000]
+        }, 
+        {
+            moveTo: [520,570],
+            lineTo: [510,580],
+            t: [2000]
+        }    
+    ],
+    
+  tStart = 500;
+    
+drawMap.forEach(function(el, i) {
+    setTimeout(function() {   
+
+        canvas.moveTo(el.moveTo[0], el.moveTo[1]);
+        canvas.lineTo(el.lineTo[0], el.lineTo[1]);        
+        canvas.stroke();
+        console.log(i);
+    }, 
+  //el.t
+    tStart
+  );
+  tStart += 120;
+});
+
+
+
+
+
+
+
+
+// INSERTING IMAGE IN CANVAS 
 
 var context = document.getElementById('canvas').getContext('2d');
 
@@ -100,5 +262,7 @@ var image = document.getElementById('image');
         image.style.display = 'none';       
     }
     image.src = 'img/brain.svg';
+
+
 
 
