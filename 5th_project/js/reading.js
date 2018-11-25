04de4ffe6213991905ffe0912c7dad2a2b9bd129
AlexanderWin <alex.win.portfolio@gmail.com>
2018-11-25 10:40:33 +0900

    build_25_11_2018_1040

---
+++ 5th_project/js/reading.js
@@ -0,0 +1,17 @@
+		function changeClass_2(){
+			if(document.getElementById("block_2").className == "block_2"){
+				document.getElementById("block_2").className += " rotated";
+			}
+			else{
+				document.getElementById("block_2").className = "block_2";
+			}
+		}
+
+		function changeClass(){
+			if(document.getElementById("block").className == "block"){
+				document.getElementById("block").className += " rotated";
+			}
+			else{
+				document.getElementById("block").className = "block";
+			}
+		}
