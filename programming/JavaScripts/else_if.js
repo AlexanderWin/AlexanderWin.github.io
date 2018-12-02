25b8b09cefa1e6e3c5d59e7ce608c0800fd268d9
AlexanderWin <alex.win.portfolio@gmail.com>
2018-12-02 11:21:31 +0900

    JS

---
+++ programming/JavaScripts/else_if.js
@@ -0,0 +1,11 @@
+color = 3;
+
+if (color == 1) {
+	document.write("Yellow");
+}	
+else if (color == 2) {
+	document.write("white");
+}	
+else {
+	document.write("red");
+}
---
+++ programming/JavaScripts/if_else.js
@@ -0,0 +1,8 @@
+var numberone = 21;
+var numbertwo = 20;
+if (numberone < numbertwo) {
+	document.write('HelloWorld');
+}
+else{
+	document.write('GoodByuWorld');
+}
