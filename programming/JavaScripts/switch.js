59a8fc499532dcb7f62388afcce001357ac663c6
AlexanderWin <alex.win.portfolio@gmail.com>
2018-12-02 11:15:50 +0900

    conditions and cycles

---
+++ programming/JavaScripts/switch.js
@@ -0,0 +1,15 @@
+var a = 4;
+switch(a){
+	case 1:
+		document.write("one");
+		break;	
+	case 2:
+		document.write("two");
+		break;	
+	case 3:
+		document.write("three");
+		break;	
+	default:
+		document.write("NoN");
+		break;	
+}
