9a9f0fc8f3dd0c7ba0345bdba7aad220b6c5029c
AlexanderWin <alex.win.portfolio@gmail.com>
2018-12-07 13:26:31 +0900

    atribute manipulation

---
+++ programming/JavaScript/DOM/attribute_change.js
@@ -0,0 +1,5 @@
+var el = document.getElementById("images");
+el.src = "img/images.jpg";
+
+var elem = document.getElementsByTagName("a");
+elem[0].href = "https://www.rambler.ru/";
---
+++ programming/JavaScript/DOM/style_change.js
@@ -0,0 +1,4 @@
+var x = document.getElementById('style');
+	x.style.color = "red";
+	x.style.height = "200px";
+	x.style.border = "1px solid black";
