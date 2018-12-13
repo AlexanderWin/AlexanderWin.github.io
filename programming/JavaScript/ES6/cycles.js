8ba70dd3603b2d6e66a75ed8cfd38445b7f24a5d
AlexanderWin <alex.win.portfolio@gmail.com>
2018-12-13 18:10:36 +0900

    ES6_cycles

---
+++ programming/JavaScript/ES6/cycles.js
@@ -0,0 +1,13 @@
+let obj ={'hello my main is': 1, b: 2, '2+3': 3};
+for (let v in obj) {
+    console.log(v);
+}
+
+let list = ["x", "y", "z"];
+for (let val of list) {
+    console.log(val);
+}
+
+for (let ch of "he123") {
+    console.log(ch);
+}
