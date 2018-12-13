9f988b7fbbb9620e3a02577f8c04f2029be500fb
AlexanderWin <alex.win.portfolio@gmail.com>
2018-12-13 18:41:24 +0900

    function

--- programming/JavaScript/ES6/function.js
+++ programming/JavaScript/ES6/function.js
@@ -6,4 +6,21 @@ add(20, 10);
 add(40, 50);
 
 const breakfest = x => "Welcome " + x;
-alert(breakfest("Alex"))
+alert(breakfest("Alex"));
+
+//forEach 
+
+const arr = [2, 3, 4, 7];
+
+arr.forEach(v => {
+    console.log(v * 2);
+});
+
+//parameters
+
+const test = (a, b = 3, c = 5) => {
+  
+    return a + b + c;    
+}
+console.log(test(12));
+
