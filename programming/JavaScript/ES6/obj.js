9129b9522ca9745dfc6dc5d1b2020db26edeb1b2
AlexanderWin <alex.win.portfolio@gmail.com>
2018-12-13 19:09:45 +0900

    ES6_obj

---
+++ programming/JavaScript/ES6/obj.js
@@ -0,0 +1,20 @@
+let tree = {
+    height: 10,
+    color: 'green',
+    grow() {
+        this.height += 2;
+    }
+};
+tree.grow();
+console.log(tree.height);
+console.log(tree.color);
+
+let height = 5;
+let width = 100;
+
+let athlete = {
+    height,
+    width
+};
+console.log(athlete.height);
+console.log(athlete.width);
