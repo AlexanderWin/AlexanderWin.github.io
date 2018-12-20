0f0b57a2b00e0c05398757a75abe7b02d7df5667
AlexanderWin <alex.win.portfolio@gmail.com>
2018-12-20 22:50:18 +0900

    task_1

---
+++ programming/JavaScript/tasks/main.js
@@ -0,0 +1,15 @@
+//Задача 1 - Пользователь вводит два числа. Найти и вывести максимальное из двух чисел.
+
+
+
+function more() {
+
+    var x = (document.getElementById('num_1').value);
+    var y = (document.getElementById('num_2').value);
+        if(x > y){
+            alert(x);
+        }
+        else{
+            alert(y);
+        }
+    }
